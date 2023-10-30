bbbe8764105836fb60855f613a6a2c80b270e558
João Pedro <jpedrorodriguescosta@gmail.com>
2023-10-29 22:04:31 -0300

    mapping endpoints paths

---
+++ node/inicializa.bat
@@ -0,0 +1,5 @@
+path Disco:\PastaOndeEstáOInstantClient; Disco:\PastaOndeEstáONodeJS
+set NODE_ORACLEDB_CONNECTIONSTRING=localhost/xe
+set NODE_ORACLEDB_USER=SYSTEM
+set NODE_ORACLEDB_PASSWORD=SuaSenha
+
